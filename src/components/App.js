957bd599870ff8ceafaac2d2ec4bba6fdb64032d
Kyle Chambers <kylejchambers@gmail.com>
2018-04-03 20:15:08 -0700

    changed initial state of app to look for silly videos

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 import changeVideo from '../actions/currentVideo.js';
 import changeVideoList from '../actions/videoList.js';
+import handleVideoSearch from '../actions/search.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import store from '../store/store.js';
 
@@ -16,8 +17,7 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    store.dispatch(changeVideo(exampleVideoData[0]));
-    store.dispatch(changeVideoList(exampleVideoData));
+    store.dispatch(handleVideoSearch('silly videos'))
   }
 
 
@@ -39,61 +39,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-
-/////////////////////////
-
-
-// export default class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       videos: [],
-//       currentVideo: null
-//     };
-//   }
-
-//   componentDidMount() {
-//     this.getYouTubeVideos('react tutorials');
-//   }
-
-//   handleVideoListEntryTitleClick(video) {
-//     this.setState({currentVideo: video});
-//   }
-
-//   getYouTubeVideos(query) {
-//     var options = {
-//       key: this.props.API_KEY,
-//       query: query
-//     };
-
-//     this.props.searchYouTube(options, (videos) =>
-//       this.setState({
-//         videos: videos,
-//         currentVideo: videos[0]
-//       })
-//     );
-//   }
-
-//   //TODO: swap out the React components below for the container components
-//   //  you wrote in the 'containers' directory.
-//   render() {
-//     return (
-//       <div>
-//         <Nav handleSearchInputChange={this.getYouTubeVideos.bind(this)}/>
-//         <div className="row">
-//           <div className="col-md-7">
-//             <VideoPlayer video={this.state.currentVideo}/>
-//           </div>
-//           <div className="col-md-5">
-//             <VideoList
-//               handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-//               videos={this.state.videos}
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
